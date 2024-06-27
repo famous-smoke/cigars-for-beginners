c6c723f45f48d0765ea8a8d7ff84037a64c6a2c2
Tony Klapatch <11136895+tonyklapatch@users.noreply.github.com>
2024-06-27 15:08:41 -0400

    Add header styling, fonts, basic sizing (#12)
    * Add header styling, fonts, basic sizing
    
    * Fix linting issues
    
    * Add the favicon
    
    * Add z-index to mobile header
    
    * Revert hero h1 to white, set font size

--- cigars-for-beginners/blocks/header/header.js
+++ cigars-for-beginners/blocks/header/header.js
@@ -2,7 +2,7 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 // media query match that indicates mobile/tablet width
-const isDesktop = window.matchMedia('(min-width: 900px)');
+const isDesktop = window.matchMedia('(min-width: 1024px)');
 
 function closeOnEscape(e) {
   if (e.code === 'Escape') {
@@ -87,13 +87,62 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 }
 
 /**
+ * Create Famous smoke and cigar for beginner logos
+ * @param block
+ * @return {HTMLDivElement}
+ */
+function createLogos(block) {
+  // Create a wrapper div
+  const wrapperDiv = document.createElement('div');
+  wrapperDiv.className = 'logo-wrapper';
+
+  // Create img elements for the logos
+  const famousLogoLink = document.createElement('a');
+  famousLogoLink.href = '/cigars-for-beginners';
+  const famousLogo = document.createElement('img');
+  famousLogo.src = '/cigars-for-beginners/icons/logo.png';
+  famousLogo.alt = 'Famous Smoke Shop Logo';
+  famousLogo.className = 'famous-logo';
+  famousLogoLink.appendChild(famousLogo);
+
+  const siteLogoLink = document.createElement('a');
+  siteLogoLink.href = '/cigars-for-beginners';
+  const siteLogo = document.createElement('img');
+  siteLogo.src = '/cigars-for-beginners/icons/bgts-logo.png';
+  siteLogo.alt = 'The Beginner\'s Guide to Cigar Smoking';
+  siteLogo.className = 'site-logo';
+  siteLogoLink.appendChild(siteLogo);
+
+  // Append img elements to the wrapper div
+  wrapperDiv.appendChild(famousLogoLink);
+  wrapperDiv.appendChild(siteLogoLink);
+
+  block.querySelector('.nav-brand').remove();
+
+  return wrapperDiv;
+}
+
+/**
+ * Check if nav section is the current page
+ * @param navSection
+ */
+function highlightActive(navSection) {
+  const currentPath = window.location.pathname;
+  const link = new URL(navSection.firstElementChild.href);
+
+  if (currentPath === link.pathname) {
+    navSection.classList.add('active');
+  }
+}
+
+/**
  * loads and decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/cigars-for-beginners/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
@@ -118,6 +167,7 @@ export default async function decorate(block) {
   const navSections = nav.querySelector('.nav-sections');
   if (navSections) {
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
+      highlightActive(navSection);
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
       navSection.addEventListener('click', () => {
         if (isDesktop.matches) {
@@ -142,8 +192,15 @@ export default async function decorate(block) {
   toggleMenu(nav, navSections, isDesktop.matches);
   isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
 
+  const navContentWrapper = document.createElement('div');
+  navContentWrapper.classList.add('nav-content-wrapper');
+  navContentWrapper.append(nav);
+
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
-  navWrapper.append(nav);
+  navWrapper.append(navContentWrapper);
+
   block.append(navWrapper);
+  const logos = createLogos(block);
+  block.firstElementChild.firstElementChild.prepend(logos);
 }
