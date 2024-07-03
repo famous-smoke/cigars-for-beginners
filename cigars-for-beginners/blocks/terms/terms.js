64782a74bea9606377884d56db534fe285d770ec
Bryan Deffley <bdeffley@famous-smoke.com>
2024-07-03 14:59:23 -0400

    import for terms section, inital setup for tabs and accordion for terms

---
+++ cigars-for-beginners/blocks/terms/terms.js
@@ -0,0 +1,101 @@
+import { toClassName } from '../../scripts/aem.js';
+
+function hasWrapper(el) {
+  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+}
+
+function tab(block) {
+  // build tablist
+  const tablist = document.createElement('div');
+  tablist.className = 'tabs-list';
+  tablist.setAttribute('role', 'tablist');
+
+  // decorate tabs and tabpanels
+  const tabs = [...block.children].map((child) => child.firstElementChild);
+  tabs.forEach((tab, i) => {
+    const id = toClassName(tab.textContent);
+
+    // decorate tabpanel
+    const tabpanel = block.children[i];
+    tabpanel.className = 'tabs-panel';
+    tabpanel.id = `tabpanel-${id}`;
+    tabpanel.setAttribute('aria-hidden', !!i);
+    tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
+    tabpanel.setAttribute('role', 'tabpanel');
+    if (!hasWrapper(tabpanel.lastElementChild)) {
+      tabpanel.lastElementChild.innerHTML = `<p>${tabpanel.lastElementChild.innerHTML}</p>`;
+    }
+
+    // build tab button
+    const button = document.createElement('button');
+    button.className = 'tabs-tab';
+    button.id = `tab-${id}`;
+    button.innerHTML = tab.innerHTML;
+    button.setAttribute('aria-controls', `tabpanel-${id}`);
+    button.setAttribute('aria-selected', !i);
+    button.setAttribute('role', 'tab');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', () => {
+      block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
+        panel.setAttribute('aria-hidden', true);
+      });
+      tablist.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('aria-selected', false);
+      });
+      tabpanel.setAttribute('aria-hidden', false);
+      button.setAttribute('aria-selected', true);
+    });
+    tablist.append(button);
+    tab.remove();
+  });
+
+  block.prepend(tablist);
+}
+
+function accordian(block) {
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...label.childNodes);
+    if (!hasWrapper(summary)) {
+      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
+    }
+    // decorate accordion item body
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
+    if (!hasWrapper(body)) {
+      body.innerHTML = `<p>${body.innerHTML}</p>`;
+    }
+    // decorate accordion item
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, body);
+    row.replaceWith(details);
+  });
+}
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
+    const label = row.children[0];
+    const summary = document.createElement('summary');
+    summary.className = 'accordion-item-label';
+    summary.append(...label.childNodes);
+    if (!hasWrapper(summary)) {
+      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
+    }
+    // decorate accordion item body
+    const body = row.children[1];
+    body.className = 'accordion-item-body';
+    if (!hasWrapper(body)) {
+      body.innerHTML = `<p>${body.innerHTML}</p>`;
+    }
+    // decorate accordion item
+    const details = document.createElement('details');
+    details.className = 'accordion-item';
+    details.append(summary, body);
+    row.replaceWith(details);
+  });
+}
--- tools/importer/import.js
+++ tools/importer/import.js
@@ -5,7 +5,48 @@
  * Import Pages as subfolders
  */
 
+// Create the Cigar Terms
+const createTermsBlock = (post, document) => {
+  const termsSection = document.getElementById('cigar-terminology');
+  const termsDiv= termsSection.firstElementChild;
+  const terms = document.getElementById('cigar-terms');
+  const termWords = terms.getElementsByTagName('h4');
+
+  if (!termsSection || termsSection.length === 0) {
+    // no terms to add
+    return;
+  }
+
+  const cell = [['Terms']];
+
+  for (const word of termWords) {
+    let definition = word.nextElementSibling;
+    cell.push([[word.textContent], [definition.textContent]]);
+  }
+
+  const block = WebImporter.DOMUtils.createTable(cell, document);
+  //post.append(block);
+
+  console.log(termsDiv);
+
+  termsDiv.insertBefore(block, terms);
+
+  // remove letters navigation, we will rebuild it
+  const letterNav = termsSection.getElementsByClassName('letter-navigation');
+  letterNav[0].remove();
+
+  if (document.getElementById('cigar-terms')) {
+    document.getElementById('cigar-terms').remove();
+  }
+}
+
 export default {
+  transformDOM: ({ document }) => {
+    const post = document.getElementById('main');
+    createTermsBlock(post, document);
+    return post;
+  },
+
   generateDocumentPath: ({ document, url, html, params }) => {
     let documentPath = document.querySelector("link[rel='canonical']").getAttribute('href');
     documentPath = WebImporter.FileUtils.sanitizePath(documentPath);
