0bea906042a77e8db39f8c587fd491a31c8f7bba
Tony Klapatch <11136895+tonyklapatch@users.noreply.github.com>
2024-07-22 08:04:54 -0400

    20 video list block (#27)
    * Add styling, modal for video list block
    
    * Add import for video list blocks, iframe options and styling
    
    * Fix gifs not loading from document

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -1,11 +1,58 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+import videoRedirects from './video-redirects.js';
+
 /*
- * AEM Generic Import Script
- * Import Pages as subfolders
+  create the video list blocks
  */
+const createVideoListBlock = (page, document) => {
+  // check if there are any videos on this page
+  const videos = document.querySelectorAll('.video-block');
+  const videoItems = [];
+  videoItems.push([['Video-list']]);
+  if (!videos || videos.length === 0) {
+    return;
+  }
+
+  const redirects = videoRedirects();
+  videos.forEach((video) => {
+    const icon = video.querySelector('a img:last-child');
+    const title = video.querySelector('h4').innerText.trim();
+    const description = video.querySelector('p').innerText.trim();
+    const videoModal = document.getElementById(video.querySelector('a').href.split('#')[1]);
+    let link = videoModal.querySelector('iframe');
+    if (link) {
+      link = link.src;
+
+      // check if this is a YouTube URL that should be changed to Vimeo
+      if (link in redirects) {
+        link = redirects[link];
+      }
+    } else {
+      link = videoModal.querySelector('img');
+    }
+
+    const row = [[icon], [title], [description], [link]];
+
+    videoItems.push(row);
+  });
+
+  const videoList = WebImporter.DOMUtils.createTable(videoItems, document);
+
+  const videoRows = document.querySelector('.video-rows');
+  videoRows.replaceWith(videoList);
+
+  // remove .video-rows from page because we just added it manually
+  WebImporter.DOMUtils.remove(page, ['.video-rows']);
+};
 
 export default {
+  transformDOM: ({ document }) => {
+    const page = document.getElementById('content');
+    createVideoListBlock(page, document);
+    return page;
+  },
+
   generateDocumentPath: ({ document, url, html, params }) => {
     let documentPath = document.querySelector("link[rel='canonical']").getAttribute('href');
     documentPath = WebImporter.FileUtils.sanitizePath(documentPath);
---
+++ tools/importer/video-redirects.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+
+/*
+  YouTube to Vimeo link conversions
+  Uses the current embed YT links on page on cigarsforbeginners.famous-smoke.com,
+  converts them to their respective vimeo embed URL
+*/
+export default function () {
+  return {
+    // how to smoke a cigar
+    'https://www.youtube.com/embed/L1qed_HIKjw?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488271440?h=31ed610b6a',
+    // basic tools a cigar smoker needs
+    'https://www.youtube.com/embed/tnTRXnxQaQ0?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488270124?h=5d13cd5f79',
+    // to inhale or not inhale a cigar
+    'https://www.youtube.com/embed/YKnDfVVar9A?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488270853?h=ac958f63a6',
+    // getting started with cigars for beginners
+    'https://www.youtube.com/embed/HhBAdyNxNgQ?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/978026565?h=ff97b371cc',
+    // cigars for beginners introduction
+    'https://www.youtube.com/embed/MCzCDUrdz8o?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/978023647?h=1054f8db64',
+    // developing your taste for cigars
+    'https://www.youtube.com/embed/eVCo-mPR1BE?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/978039197?h=a340c4d5e4',
+    // how to choose the best first cigar
+    'https://www.youtube.com/embed/c1meHDzQXrU?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488594025?h=cc1ddc9543',
+    // flavored vs. traditional cigars
+    'https://www.youtube.com/embed/2vWMBM5QXV8?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488597805?h=02cc62ef84',
+    // what determines cigar strength
+    'https://www.youtube.com/embed/SxvkK-yqLxs?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488268438?h=f264b92697',
+    // how to get rid of cigar odor and breath
+    'https://www.youtube.com/embed/fzKl_A920eo?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488596650?h=89a84ccf7f',
+    // do cigars have nicotine and how much
+    'https://www.youtube.com/embed/64aAhMnC0U8?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488595723?h=13c1ab0930',
+    // how to clean your palate
+    'https://www.youtube.com/embed/7l3yd2mgxRg?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488275624?h=99ba1199af',
+    // develop your collection of cigars
+    'https://www.youtube.com/embed/B4rF-yvor0k?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/978028521?h=9cf4947914',
+    // proper care and storage for your cigars
+    'https://www.youtube.com/embed/fRPMszQJVsE?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/488598721?h=31ed610b6a',
+    // how to season a humidor
+    'https://www.youtube.com/embed/vhg6gyz3L8s?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488272115?h=c78298310b',
+    // how to calibrate a hygrometer
+    'https://www.youtube.com/embed/y1zP5W43ZUQ?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488594835?h=3456e56151',
+    // getting started with cigar pairing
+    'https://www.youtube.com/embed/2XBBbmJ1tyI?rel=0&controls=0&showinfo=0&?modestbranding=0': 'https://player.vimeo.com/video/978029600?h=3256489373',
+    // how to make an old-fashioned
+    'https://www.youtube.com/embed/G38rF3fbU3s?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488220776?h=d9f9755f90',
+    // how to make a mojito
+    'https://www.youtube.com/embed/7tGNGN0PHRQ?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488220150?h=8c2f6c0072',
+    // how to make a manhattan
+    'https://www.youtube.com/embed/9BHkLrsHZPg?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488219239?h=7def245a23',
+    // how to make a havana
+    'https://www.youtube.com/embed/3fpqfycXHCE?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488218426?h=cd9691b481',
+    // how to make a flaming dr. pepper
+    'https://www.youtube.com/embed/2Fyv_i5HJng?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488217962?h=a64c1fd609',
+    // how to make a dark and stormy
+    'https://www.youtube.com/embed/gZMt_pdwQRg?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488217299?h=b15cc18ea3',
+    // how to make a cuba libre
+    'https://www.youtube.com/embed/840WP0VyJ8Y?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488216916?h=1828ff72fd',
+    // how to make a bloody mary
+    'https://www.youtube.com/embed/CRb1tGzpdnw?rel=0&controls=0&showinfo=0&branding=0': 'https://player.vimeo.com/video/488215678?h=397446533a',
+  };
+}
