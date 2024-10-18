4a570793c3d5665aced7d0136f8f59ce2b970dfb
Tony Klapatch <aklapatch@famous-smoke.com>
2024-10-18 11:30:43 -0400

    Display hero behind nav, remove video summary embed title

--- cigars-for-beginners/blocks/video-summary/video-summary.js
+++ cigars-for-beginners/blocks/video-summary/video-summary.js
@@ -39,18 +39,14 @@ function getShowHide() {
 export default async function decorate(block) {
   const url = block.querySelector('a');
 
-  const title = block.querySelector('h3');
-  title.className = 'video-summary-embed-title';
-
   const img = block.querySelector('img');
   img.className = 'video-summary-embed';
 
-  const facadeWrap = title.parentElement;
+  const facadeWrap = img.parentElement;
   facadeWrap.className = 'video-summary-embed-facade';
   facadeWrap.addEventListener('click', () => {
     const embed = getEmbed(url.innerText);
     facadeWrap.replaceWith(embed);
-    title.remove();
   });
 
   url.remove();
